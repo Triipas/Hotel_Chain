48f490e8192df3013bffcd719d2647da48749e4b
Andy-2214 <markxx32@gmail.com>
2025-10-25 17:52:34 -0500

    prueba

--- Configuration/DatabaseConfiguration.cs
+++ Configuration/DatabaseConfiguration.cs
@@ -8,10 +8,22 @@ namespace Hotel_chain.Configuration
     {
         public static IServiceCollection ConfigureDatabase(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddDbContext<AppDbContext>(options =>
-                options.UseMySQL(configuration.GetConnectionString("DefaultConnection") ?? 
-                                throw new InvalidOperationException("Connection string 'DefaultConnection' not found."))
-            );
+            // Primero intenta leer variable de entorno
+            var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
+
+            if (!string.IsNullOrEmpty(connectionString))
+            {
+                // Usar cadena de conexión de Render (AWS)
+                services.AddDbContext<AppDbContext>(options =>
+                    options.UseMySQL(connectionString));
+            }
+            else
+            {
+                // Usar appsettings.json (localhost)
+                services.AddDbContext<AppDbContext>(options =>
+                    options.UseMySQL(configuration.GetConnectionString("DefaultConnection") ??
+                                     throw new InvalidOperationException("Connection string 'DefaultConnection' not found.")));
+            }
 
             return services;
         }
